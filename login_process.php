9e7b8986ce5905cd9bc9129dd394cd8658bd42a1
sinaa07 <151539519+sinaa07@users.noreply.github.com>
2024-10-29 19:31:55 +0530

    Update login_process.php
    added queries for finding user and redirecting to myacc or signup page based on result

--- login_process.php
+++ login_process.php
@@ -1,12 +1,17 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
 include 'connection.php';
-$email = $_POST["name"];
+$email = $_POST["email"];
 $password = $_POST["password"];
 
-$query = "select password from users where email='$email';"
-$result = mysqli_query($connect,$query);
+$query = "select password from users where email='$email';";
+$result = mysqli_query($conn,$query);
 $login_status=false;
+$user_exist=false;
 if( mysqli_num_rows($result) > 0 ){
+    $user_exist=true;
     $row = mysqli_fetch_assoc($result);
     $db_password = $row["password"];
     if($db_password == $password ){
@@ -15,23 +20,29 @@ if( mysqli_num_rows($result) > 0 ){
     }
     else{
         $login_status=false;
-
     }
 }
 else{
-    echo "No user found.";
+    $user_exist=false;
 }
 ?>
 <html>
 <body>
     <script>
-    const logged= <?php echo json_encode($login_status); ?>
+    const logged = <?php echo json_encode($login_status); ?>;
+    const exist = <?php echo json_encode($user_exist); ?>;
     if(logged){
-      window.location.href(myacc.html);
+      window.location.href = "myacc.html";
     }
     else {
+        if(exist){
             alert("Invalid login credentials.");
+            window.location.href = "login.php";
         }
+        else{
+            window.location.href = "signup.html";
+        }
+    }
   </script>
 </body>
 </html>
