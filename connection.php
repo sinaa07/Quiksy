41cd061421151d78791e0edbe52b32e1dad2bf42
sinaa07 <151539519+sinaa07@users.noreply.github.com>
2024-10-29 19:30:52 +0530

    added "use quiksy" query

--- connection.php
+++ connection.php
@@ -11,4 +11,9 @@ if(!$conn){
     die("Connection Failed: ". mysqli_connect_error());
 }
 echo "Connected successfully";
+$db_query = "use quiksy;";
+$res = mysqli_query($conn,$db_query);
+if (!$res) {
+    die("Query execution failed: " . mysqli_error($conn));
+}
 ?>
